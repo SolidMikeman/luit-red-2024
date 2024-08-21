a9b4a7bcd2ff15061984d51ab557251b79c1d636
SolidMikeman <superkamiguru1998@gmail.com>
2024-08-21 00:43:34 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,6 @@
 # python program to print "Hello World" 
 print("Hello World My Name is Python")
-print("How are you today?")
+print("How are you today?") 
+# python program to print "Hello World" 
+message = "We will have so much fun learning together!"
+print(message) 
