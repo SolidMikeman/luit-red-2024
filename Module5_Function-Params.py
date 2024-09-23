c1d7b8955c9430a0e6b4b61dcfa30aa591313465
SolidMikeman <superkamiguru1998@gmail.com>
2024-09-22 22:32:52 -0400

    Introduction to Creating Custom Functions in Python
    Summary
    This pull request introduces the concept of user-defined functions in Python, covering the basics of defining, calling, and using parameters within functions. The examples provided illustrate how to encapsulate code into reusable blocks and demonstrate the benefits of using parameters to pass data into functions.
    
    Key Features
    Basic Function Definition and Call:
    
    A simple function named greet is defined to print a greeting message, showcasing the most basic form of function creation and invocation.
    Using Parameters in Functions:
    
    Introduces how to define functions that accept parameters, allowing for flexible and reusable code.
    Demonstrated with a function get_average that calculates the average of a list of numbers provided as a parameter.
    Returning Values:
    
    The concept of returning values from functions is explained, with get_average returning the calculated average to the caller.
    Counting Specific Elements in a String:
    
    A function print_letter_count is created to count and display the occurrences of a specific letter in a given string, showcasing the practical use of loops and conditionals within functions.
    Testing
    Verified that each function correctly performs its intended task.
    Ensured that the functions handle a variety of inputs and provide accurate outputs.

---
+++ Module5_Function-Params.py
@@ -0,0 +1,21 @@
+#Parameters are various values passed to a function
+
+input_numbers = [5.0, 3.5, 7.8, 9.9, 10.0]
+
+def get_average(input_numbers):  # This is a parameter
+    total_sum = 0.0  # Avoid using 'sum' as it is a built-in function
+    for number in input_numbers:
+        total_sum += number
+    return total_sum / len(input_numbers)
+
+average = get_average(input_numbers)  # Call the function to get the average
+print(average)
+
+def print_letter_count(text, letter):
+    counter = 0
+    for char in text:
+        if char == letter:
+            counter += 1
+    print('Number of', letter, 'is', counter)
+
+print_letter_count('Welcome', 'e')
---
+++ Module5_Intro-Functions.py
@@ -0,0 +1,6 @@
+#Intro to creating your own functions
+
+def greet():
+    print('Hello my dear!')
+
+greet()
